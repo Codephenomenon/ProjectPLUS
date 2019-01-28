e68db6cd53af16211622a9bd32f03e85f017680c
Daniel Clayton <dclaytondev@gmail.com>
2019-01-28 16:00:58 -0600

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Hello World</h2>
+        <a href="/auth/google">Sign in with Google</a>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+  facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+  cookieKeyA: process.env.COOKIE_KEY_A,
+  cookieKeyB: process.env.COOKIE_KEY_B,
+  cookieKeyC: process.env.COOKIE_KEY_C,
+  mongoURI: process.env.MONGO_URI
+};
