d40e5305785e753ccb73426b29e67859705e4d49
Daniel Clayton <dclaytondev@gmail.com>
2019-05-02 00:40:18 -0500

    testing build

---
+++ client/build/precache-manifest.8c611ea88aab6d06b6afbb93f1f9b176.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1683ac60f7e8f46854aa",
+    "url": "/static/js/main.1683ac60.chunk.js"
+  },
+  {
+    "revision": "a48f19cc99761f9bdb28",
+    "url": "/static/js/1.a48f19cc.chunk.js"
+  },
+  {
+    "revision": "cc401a022cad503b415cab6f7ba6f906",
+    "url": "/index.html"
+  }
+];
